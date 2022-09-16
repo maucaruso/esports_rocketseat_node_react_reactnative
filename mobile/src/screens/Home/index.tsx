92c14c43f1e0a969af1fb8a38f4c011449068ba1
maucaruso <carusojr@outlook.com>
2022-09-16 07:01:28 -0300

    mobile game screen

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Image, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from '@react-navigation/native';
 
 import logoImg from "../../assets/logo-nlw-esports.png";
 import { Background } from "../../components/Background";
@@ -11,6 +12,12 @@ import { styles } from "./styles";
 
 export function Home() {
   const [games, setGames] = useState<GameCardProps[]>([]);
+  
+  const navigation = useNavigation();
+  
+  function handleOpenGame({ id, title, bannerUrl }: GameCardProps) {
+    navigation.navigate("game", { id, title, bannerUrl });
+  }
 
   useEffect(() => {
     fetch("http://192.168.0.18:3333/games")
@@ -31,7 +38,7 @@ export function Home() {
         <FlatList
           data={games}
           keyExtractor={(item) => item.id}
-          renderItem={({ item }) => <GameCard data={item} />}
+          renderItem={({ item }) => <GameCard data={item} onPress={() => handleOpenGame(item)} />}
           horizontal
           showsHorizontalScrollIndicator={false}
           contentContainerStyle={styles.contentList}
